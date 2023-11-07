a50160cd75f4b914763e481598c1d0080be9a91e
Milton Castelli Veiga <castellimus@gmail.com>
2023-11-07 11:16:06 -0300

    aula 07/11 - Service, AutoIncrement, Delete

--- src/main/java/br/com/fatec/hellospringboot/entities/Client.java
+++ src/main/java/br/com/fatec/hellospringboot/entities/Client.java
@@ -1,13 +1,17 @@
 package br.com.fatec.hellospringboot.entities;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 
 @Entity
 @Table(name= "TBL_CLIENT")
 public class Client {
+
     @Id
+    @GeneratedValue (strategy = GenerationType.IDENTITY)
     private int id;
     
     private String name;
